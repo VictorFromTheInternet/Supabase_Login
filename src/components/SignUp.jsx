46e81588979941eac08a88581aa5b93d9042f002
VictorFromTheInternet <vma0430@gmail.com>
2025-07-20 16:53:22 -0500

    trying to setup supabase

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
+import {UserAuth} from '../context/AuthContext'
+
 
 function SignUp() {
-  return (
-    <div>
-        <form className="max-w-md m-auto pt-24">
-            <h2 className="font-bold pb-2">Sign up today!</h2>
-            <p className="">
-                Already have an account? <Link to='/signin'>Sign in!</Link> 
-            </p>
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState('')
+    const [error, setError] = useState('')
 
-            <div className="flex flex-col py-4">
-                <input type="email" name="email" id="email" className="p-3 mt-4" />
+    const {session} = UserAuth()
+    console.log(session)
 
-                <input type="password" name="password" id="password" className="p-3 mt-4" />
+    return (
+        <div>
+            <form className="max-w-md m-auto pt-24">
+                <h2 className="font-bold pb-2">Sign up today!</h2>
+                <p className="">
+                    Already have an account? <Link to='/signin'>Sign in!</Link> 
+                </p>
 
-                <button className="mt-4">Sign Up</button>
-            </div>
-        </form>        
-    </div>
-  )
+                <div className="flex flex-col py-4">
+                    <input placeholder="Email" type="email" name="email" id="email" className="p-3 mt-4" />
+
+                    <input placeholder="Password" type="password" name="password" id="password" className="p-3 mt-4" />
+
+                    <button type="submit" disabled={loading} className="mt-4">Sign Up</button>
+                </div>
+            </form>        
+        </div>
+    )
 }
 
 export default SignUp
