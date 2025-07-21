4e791d72b8540df0fc3113c0f8e0d6460498be6a
VictorFromTheInternet <vma0430@gmail.com>
2025-07-20 21:32:55 -0500

    protecting routes

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {UserAuth} from '../context/AuthContext'
+import {Navigate} from 'react-router-dom'
+
+function PrivateRoute({children}) {
+    const {session} = UserAuth()
+
+    if(session){
+        return <p>Loading...</p>
+    }
+
+    return <>{ session ? <>{children}</> : <Navigate to="/signup" />}</>        
+    
+}
+
+export default PrivateRoute
--- src/router.jsx
+++ src/router.jsx
@@ -3,11 +3,11 @@ import App from './App'
 import SignUp from "./components/SignUp"
 import SignIn from "./components/SignIn"
 import Dashboard from "./components/Dashboard"
-
+import PrivateRoute from "./components/PrivateRoute"
 
 export const router = createBrowserRouter([
     {path: "/", element: <App/>},
     {path: "/signin", element: <SignIn/>},
     {path: "/signup", element: <SignUp/>},
-    {path: "/dashboard", element: <Dashboard/>}
+    {path: "/dashboard", element: <PrivateRoute> <Dashboard/> </PrivateRoute>  }
 ])
